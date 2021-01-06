a57e88c578e7ed4322bb2dab0cc1bde8d16899cc
Shikhar-code <73746913+Shikhar-code@users.noreply.github.com>
2021-01-06 21:12:50 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,58 @@
+var dog, happyDog, database, foodS, foodStock , position;
+var backgroundImg, dogImg;
+
+function preload()
+{
+  dogImg = loadImage("images/dogImg.png");
+  happyDog = loadImage("images/dogImg1.png");  
+  //backgroundImg = loadImage("bg.jpg");
+  
+}
+
+
+function setup(){
+    createCanvas(500,500);
+    database= firebase.database();
+    dog = createSprite(250,350,50,50);
+    dog.addImage("dog",dogImg);
+    dog.scale=0.3  ;
+     foodStock =  database.ref('food');
+    foodStock.on("value",readStock)
+}
+
+function draw(){
+    background("lightblue");
+    
+   
+    if(keyWentDown("UP_ARROW")){
+        
+       WriteStock(foodS)
+       dog.addImage("happyDog",happyDog);   
+    }
+    
+
+   
+    
+    drawSprites();
+    textSize(25);
+    fill("black");
+    text("food remaining:"+ foodS,120,80);
+    text("press up to feed the dog!" ,120 , 120 )
+    
+}
+
+function WriteStock(petFOOD){
+    if(petFOOD<=0){
+        petFOOD=0
+    }
+    else{
+        petFOOD=petFOOD-1;
+    }
+    database.ref('/').update({
+        food:petFOOD
+    })
+}
+function readStock(data){
+    foodS = data.val();
+    
+}
